7db97110112073a18142a2adb37d491e5b15b078
vvadithya <vedulavenkataadithya@gmail.com>
2022-06-20 23:29:03 +0400

    gui development

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import eel
-
-eel.init('web')
-
-eel.start('resident_card.html')
+# importing the eel library  
+import eel  
+# initializing the application  
+eel.init("web")  
+  
+# using the eel.expose command  
+@eel.expose
+def test(name , address , nation , date_of_birth , image , gender , output_path):
+    print(name)
+    print(address)
+    print(nation)
+  
+# starting the application  
+eel.start("resident_card.html" ,size = (800,800))  
